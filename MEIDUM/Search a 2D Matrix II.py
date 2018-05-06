7d9e8482e2588c4f62fe097af17ff522e7f28e2a
jian-xu <aaronxujian0@gmail.com>
2018-05-06 19:32:26 +0800

    new one

---
+++ MEIDUM/Search a 2D Matrix II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if matrix == [] or matrix == [[]]:
+            return False
+        j = -1
+        for row in matrix:
+            while j + len(row) and row[j] > target:
+                j -= 1
+            if row[j] == target:
+                return True
+        return False
